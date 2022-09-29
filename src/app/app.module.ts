48c1a05c3f41ffa74a1eff82f3468e8c4acb041c
FilippiUnito <mattius993@gmail.com>
2022-09-29 13:09:35 +0200

    ao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ElencoFigurineComponent } from './elenco-figurine/elenco-figurine.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastComponent } from './toast/toast.component';
 
+
+export class AppBootstrapModule {}
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    TopbarComponent,
+    ElencoFigurineComponent,
+    CardDetailComponent,
+    LoginComponent,
+    ToastComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
